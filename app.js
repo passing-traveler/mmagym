f0b30a8ac275115e03441dcd5159e4d63b0f9f9f
Abel Dimas <avtw37@gmail.com>
2020-11-30 09:07:51 -0800

    animation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const menuLinks = document.querySelector(".navbar_menu");
 const navLogo = document.querySelector("#navbar_logo");
 const body = document.querySelector("body");
 
-// display mobile menu
+// Display Mobile Menu
 const mobileMenu = () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
@@ -11,3 +11,64 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener("click", mobileMenu);
+
+// Animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".animate-hero", {
+  duration: 0.6,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+});
+
+gsap.from(".animate-services", {
+  scrollTrigger: ".animate-services",
+  duration: 0.5,
+  opacity: 1,
+  x: -150,
+  stagger: 0.12,
+});
+
+gsap.from(".animate-img", {
+  scrollTrigger: ".animate-services",
+  duration: 1.2,
+  opacity: 0,
+  x: -200,
+});
+
+gsap.from(".animate-membership", {
+  scrollTrigger: ".animate-membership",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+  delay: 0.5,
+});
+
+gsap.from(".animate-card", {
+  scrollTrigger: ".animate-card",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.1,
+  delay: 0.2,
+});
+
+gsap.from(".animate-team", {
+  scrollTrigger: ".animate-team",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+  delay: 0.2,
+});
+
+gsap.from(".animate-email", {
+  scrollTrigger: ".animate-email",
+  duration: 0.8,
+  opacity: 0,
+  y: -150,
+  stagger: 0.25,
+  delay: 0.4,
+});
